9bc3ddd691dfef557014b903f874dfb35ace34f2
shahdat-sajib <shahdat35-2757@diu.edu.bd>
2025-07-21 16:54:07 +0600

    deployment ready v7

--- src/components/index.js
+++ src/components/index.js
@@ -12,6 +12,6 @@ export { default as About } from './sections/about';
 export { default as Jobs } from './sections/jobs';
 export { default as Featured } from './sections/featured';
 export { default as Projects } from './sections/projects';
-export { default as Achievements } from './sections/Achievements';
+export { default as Achievements } from './sections/achievements';
 export { default as Certifications } from './sections/certifications';
 export { default as Contact } from './sections/contact';
